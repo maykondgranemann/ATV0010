d5e98f33abc98426aabb13b64969fb30957610e6
Maykon Granemann <maykondgranemann@gmail.com>
2025-05-12 20:52:26 -0300

    Criado cadastro de dados demograficos

--- Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
+++ Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
@@ -10,6 +10,17 @@ namespace Zuplae.Aulas.Atv0010.ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("========================== ATV0010 ==========================");
+            Console.Write("Digite o nome da cidade:");
+            string cidade = Console.ReadLine();
+            Console.Write("Digite o estado:");
+            string estado = Console.ReadLine();
+            Console.Write("Digite a população:");
+            string populacao = Console.ReadLine();
+            Console.Write("Digite PIB:");
+            string pib = Console.ReadLine();
+
+            Console.WriteLine($"Os dados digitados foram: {cidade}-{estado}-{populacao}-{pib}");
         }
     }
 }
