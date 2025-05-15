e449fd5392d328156130ab0cd175ff6c0278fdd2
Maykon Granemann <maykondgranemann@gmail.com>
2025-05-15 20:22:57 -0300

    Tipos de Dados - Numerico

--- Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
+++ Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
@@ -11,16 +11,23 @@ namespace Zuplae.Aulas.Atv0010.ConsoleApp
         static void Main(string[] args)
         {
             Console.WriteLine("========================== ATV0010 ==========================");
+
             Console.Write("Digite o nome da cidade:");
             string cidade = Console.ReadLine();
+
             Console.Write("Digite o estado:");
             string estado = Console.ReadLine();
-            Console.Write("Digite a população:");
-            string populacao = Console.ReadLine();
-            Console.Write("Digite PIB:");
-            string pib = Console.ReadLine();
 
-            Console.WriteLine($"Os dados digitados foram: {cidade}-{estado}-{populacao}-{pib}");
+            Console.Write("Digite a população:");
+            int populacao = Convert.ToInt32( Console.ReadLine() );
+
+            Console.Write("Digite PIB:");
+            float pib = Convert.ToSingle(Console.ReadLine());
+           
+
+            float pibPerCapta = pib / populacao;
+
+            Console.WriteLine($"Os dados digitados foram: {cidade}-{estado}-{populacao}-{pib}-{pibPerCapta}");
         }
     }
 }
