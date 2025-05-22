3915f421a221c4577e3c4e26f0a49ca7b6095b3c
Maykon Granemann <maykondgranemann@gmail.com>
2025-05-22 20:26:42 -0300

    Adicionado List a solucao

--- Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
+++ Zuplae.Aulas.Atv0010/Zuplae.Aulas.Atv0010.ConsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlTypes;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,52 @@ namespace Zuplae.Aulas.Atv0010.ConsoleApp
         {
             Console.WriteLine("========================== ATV0010 ==========================");
 
-            Console.Write("Digite o nome da cidade:");
-            string cidade = Console.ReadLine();
+            List<Cidade> cidades = new List<Cidade>();
 
-            Console.Write("Digite o estado:");
-            string estado = Console.ReadLine();
+            bool continua = true;
+            int n_cidade = 1;
 
-            Console.Write("Digite a população:");
-            int populacao = Convert.ToInt32( Console.ReadLine() );
+            while (continua) 
+            {
+                Cidade cidade = new Cidade(); //Instanciando um objeto
+                Console.WriteLine("Digite os dados da Cidade "+ n_cidade);
+                Console.Write("Digite o nome da cidade:");
+                cidade.nome = Console.ReadLine();
 
-            Console.Write("Digite PIB:");
-            float pib = Convert.ToSingle(Console.ReadLine());
-           
+                Console.Write("Digite o estado:");
+                cidade.estado = Console.ReadLine();
 
-            float pibPerCapta = pib / populacao;
+                Console.Write("Digite a população:");
+                cidade.populacao = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine($"Os dados digitados foram: {cidade}-{estado}-{populacao}-{pib}-{pibPerCapta}");
+                Console.Write("Digite PIB:");
+                cidade.pib = Convert.ToSingle(Console.ReadLine());
+
+                cidade.pibPerCapta = cidade.pib / cidade.populacao;
+
+                cidades.Add(cidade);
+
+                Console.Write("Deseja inserir mais uma cidade? (s/n): ");
+                char resposta = Convert.ToChar( Console.ReadLine() );
+                if(resposta == 'n')
+                {
+                    continua = false;
+                }
+                else
+                {
+                    Console.WriteLine();
+                    n_cidade++;
+                }
+            }
+
+            Console.WriteLine("\n======== Imprimindo todas as cidades =========");
+            n_cidade = 1;
+            foreach (var c in cidades)
+            {
+                Console.WriteLine($"Cidade {n_cidade}: {c.nome}; {c.estado}; {c.populacao}; {c.pib}; {c.pibPerCapta}");
+                n_cidade++;
+            }
+            
         }
     }
 }
